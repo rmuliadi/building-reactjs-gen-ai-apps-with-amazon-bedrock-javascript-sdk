f0916ab8b5160f14ed287a20e0047ce9df1bed2e
rmuliadi <rmuliadi@users.noreply.github.com>
2024-03-07 16:20:11 +0700

    Update BedrockKBRetrieve.jsx

--- reactjs-gen-ai-apps/src/BedrockKBRetrieve.jsx
+++ reactjs-gen-ai-apps/src/BedrockKBRetrieve.jsx
@@ -46,7 +46,7 @@ export default ({ llm }) => {
     return (
 
         <Container key={2} disableContentPaddings={false}
-            header={<Header variant="h2">Conversacion</Header>}>
+            header={<Header variant="h2">Conversation</Header>}>
 
             <SpaceBetween size="xs">
                 <BedrockKBLoader ref={childRef} key={1} />
@@ -71,13 +71,13 @@ export default ({ llm }) => {
                 }
                 <Textarea
                     fitHeight
-                    placeholder="Escribe algo al modelo..."
+                    placeholder="Write something..."
                     onChange={changeHandler}
                     onKeyUp={event => processKeyUp(event.detail.keyCode)}
                     value={value}
                     disabled={loading}
                     inputMode="text" />
-                <Button fullWidth key={2} loading={loading} onClick={sendText} variant="primary" >Enviar</Button>
+                <Button fullWidth key={2} loading={loading} onClick={sendText} variant="primary" >Submit</Button>
             </SpaceBetween>
 
 
