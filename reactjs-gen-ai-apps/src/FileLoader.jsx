309636e112042c4be267c47431b1db5e6039b1ff
rmuliadi <rmuliadi@users.noreply.github.com>
2024-03-27 22:15:22 +0700

    Update FileLoader.jsx

--- reactjs-gen-ai-apps/src/FileLoader.jsx
+++ reactjs-gen-ai-apps/src/FileLoader.jsx
@@ -46,12 +46,12 @@ export default () => {
     return (
         <Header variant="h1"
             actions={value.length ? <SpaceBetween direction="horizontal" size="xs">
-                <Button fullWidth key={2} loading={loading} onClick={processDocuments} variant="primary" >Procesar Documentos</Button>
+                <Button fullWidth key={2} loading={loading} onClick={processDocuments} variant="primary" >Process Documents</Button>
             </SpaceBetween> : null}
         >
             <FormField key={1}
-                label="Documentos (pdf, doc, txt)"
-                description="Elija documentos a consultar"
+                label="Documents (pdf, doc, txt)"
+                description="Pick documents to consult"
             >
                 <FileUpload
                     onChange={handleOnChange}
