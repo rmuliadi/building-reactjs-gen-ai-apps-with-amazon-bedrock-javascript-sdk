b16055f00cbbadcd960891ebaad7360c1e7d3ad9
rmuliadi <rmuliadi@users.noreply.github.com>
2024-11-22 17:03:58 +0700

    Update Menu.jsx

--- reactjs-gen-ai-apps/src/Menu.jsx
+++ reactjs-gen-ai-apps/src/Menu.jsx
@@ -32,11 +32,11 @@ const Menu = ({ signOut, groups, ...user }) => {
 
       }}
       utilities={[
-        {
+        /*{
           type: "button", text: "Chat With Amazon Bedrock", 
           href: "https://aws.amazon.com/bedrock/", external: true, externalIconAriaLabel: " (opens in a new tab)"
         },
-        /*{
+        {
           type: "button", text: "Langchain.js", 
           href: "https://js.langchain.com/docs/get_started/introduction", 
           external: true, externalIconAriaLabel: " (opens in a new tab)"
@@ -46,13 +46,13 @@ const Menu = ({ signOut, groups, ...user }) => {
           href: "https://github.com/build-on-aws/building-reactjs-gen-ai-apps-with-amazon-bedrock-javascript-sdk", 
           external: true, externalIconAriaLabel: " (opens in a new tab)"
         },*/
-        {
+        /*{
           type: "menu-dropdown",
           text: "Authors",
           onItemClick: ((e) => { itemClick(e) }),
           iconName: "user-profile",
           items: [
-            /*{
+            {
               id: "Eli",
               text: "Elizabeth Fuentes Leone",
               href: "https://www.linkedin.com/in/lizfue/",
